a54d1129309c60be786b2ac46f4e53d97cda9042
Junsik Bang <jbangdev@gmail.com>
2025-02-18 12:10:38 +0900

    Add files via upload

---
+++ moveBuild.js
@@ -0,0 +1,16 @@
+﻿const fs = require('fs-extra');
+const path = require('path');
+
+(async () => {
+    const buildDir = path.join(__dirname, 'build');
+    const rootDir = __dirname;
+
+    if (fs.existsSync(buildDir)) {
+        // build 폴더 내부 파일을 루트 디렉토리로 이동
+        await fs.copy(buildDir, rootDir);
+        await fs.remove(buildDir); // build 폴더 삭제
+        console.log('✅ Build files moved successfully.');
+    } else {
+        console.error('❌ Build folder not found.');
+    }
+})();
